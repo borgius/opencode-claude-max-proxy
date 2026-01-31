05b7a744f4d3ea20e71a6c9e4de1be1056d6ac70
Victor Borg <868919+borgius@users.noreply.github.com>
2026-01-31 15:25:57 -0500

    debug: return container API info

--- src/worker.js
+++ src/worker.js
@@ -34,64 +34,19 @@ export class ClaudeContainer {
   constructor(ctx, env) {
     this.ctx = ctx;
     this.env = env;
-    this.containerStarted = false;
   }
 
   async fetch(request) {
-    try {
-      // Start container if not started
-      if (!this.containerStarted) {
-        await this.ctx.blockConcurrencyWhile(async () => {
-          if (!this.ctx.container.running) {
-            this.ctx.container.start({
-              env: {
-                CLAUDE_OAUTH_CREDS: this.env.CLAUDE_OAUTH_CREDS || '',
-                PORT: '8080'
-              }
-            });
-          }
-          this.containerStarted = true;
-        });
-      }
-
-      // Wait for container to be ready (simple retry)
-      let retries = 30;
-      while (!this.ctx.container.running && retries > 0) {
-        await new Promise(r => setTimeout(r, 1000));
-        retries--;
-      }
-
-      if (!this.ctx.container.running) {
-        return new Response(JSON.stringify({ error: 'Container failed to start' }), {
-          status: 503,
-          headers: { 'Content-Type': 'application/json' }
-        });
-      }
-
-      // Get TCP port to container
-      const port = this.ctx.container.getTcpPort(8080);
-
-      // Forward request to container
-      const containerUrl = new URL(request.url);
-      containerUrl.protocol = 'http:';
-      containerUrl.host = port.host;
-
-      const containerRequest = new Request(containerUrl, {
-        method: request.method,
-        headers: request.headers,
-        body: request.body,
-      });
-
-      return await fetch(containerRequest);
-    } catch (error) {
-      return new Response(JSON.stringify({
-        error: error.message,
-        stack: error.stack,
-        containerRunning: this.ctx.container?.running
-      }), {
-        status: 500,
-        headers: { 'Content-Type': 'application/json' }
-      });
-    }
+    // Debug: return info about the container API availability
+    return new Response(JSON.stringify({
+      debug: true,
+      hasCtx: !!this.ctx,
+      hasContainer: !!this.ctx?.container,
+      containerKeys: this.ctx?.container ? Object.keys(this.ctx.container) : [],
+      envKeys: Object.keys(this.env || {})
+    }, null, 2), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    });
   }
 }
